71f147c6e9f8e9648fdea8e1976992fe1f360bbb
AmaefuleMcDonald <amaefule720@gmail.com.com>
2024-03-03 18:02:55 +0000

    stats explanation working for all languages

--- resources/views/stats_explanation.blade.php
+++ resources/views/stats_explanation.blade.php
@@ -5,27 +5,27 @@
 <div class="container">
     <h1>Player Stats Explanation</h1>
     <div class="list-group">
-        <p class="list-group-item"><strong>Name:</strong> The name of the football player.</p>
-        <p class="list-group-item"><strong>Ratings:</strong> The overall rating of the player in FIFA 23, representing the player's overall skill level.</p>
-        <p class="list-group-item"><strong>Position:</strong> The primary playing position(s) of the player.</p>
-        <p class="list-group-item"><strong>Version:</strong> Indicates the type of card or version the player has in FUT (FIFA Ultimate Team), such as "Icon Explosive" or "Icon Controlled".</p>
-        <p class="list-group-item"><strong>PS:</strong> Presumably the price of the player on the PlayStation network. It appears some values are missing or set to 0, which could indicate unavailability or lack of data.</p>
-        <p class="list-group-item"><strong>SKI:</strong> Skill Moves rating, represented by the number of stars (1 to 5), indicating how skilled a player is at performing complex moves.</p>
-        <p class="list-group-item"><strong>WF:</strong> Weak Foot rating, also represented by the number of stars (1 to 5), indicating how effective a player is with their non-dominant foot.</p>
-        <p class="list-group-item"><strong>WR:</strong> Work Rate, shown as two values (e.g., "H/M") indicating the player's work rate in attack and defense, respectively. "H" stands for High, "M" for Medium, and "L" for Low.</p>
-        <p class="list-group-item"><strong>PAC:</strong> Pace, a measure of a player's speed and acceleration.</p>
-        <p class="list-group-item"><strong>SHO:</strong> Shooting, indicating a player's ability to score goals.</p>
-        <p class="list-group-item"><strong>PAS:</strong> Passing, representing a player's ability to distribute the ball accurately.</p>
-        <p class="list-group-item"><strong>DRI:</strong> Dribbling, measuring a player's control over the ball while moving.</p>
-        <p class="list-group-item"><strong>DEF:</strong> Defense, indicating a player's defensive skills.</p>
-        <p class="list-group-item"><strong>PHY:</strong> Physicality, representing a player's physical strength and stamina.</p>
-        <p class="list-group-item"><strong>Body:</strong> Information on the player's body type, height, and weight.</p>
-        <p class="list-group-item"><strong>Popularity:</strong> A numeric value possibly indicating the player's popularity or demand within the game.</p>
-        <p class="list-group-item"><strong>BS:</strong> Possibly "Base Stats," a cumulative score of basic player attributes.</p>
-        <p class="list-group-item"><strong>IGS:</strong> In-Game Stats, a comprehensive total of all the player's attributes within the game.</p>
-        <p class="list-group-item"><strong>Club:</strong> The club for which the player's card is issued in FUT.</p>
-        <p class="list-group-item"><strong>League:</strong> The league to which the player's club belongs in the context of FUT.</p>
-        <p class="list-group-item"><strong>Country:</strong> The nationality of the player.</p>
+        <p class="list-group-item"><strong>Name:</strong> {{ __('messages.nameexp') }}</p>
+        <p class="list-group-item"><strong>Ratings:</strong> {{ __('messages.ratingexp') }}</p>
+        <p class="list-group-item"><strong>Position:</strong>{{ __('messages.positionexp') }}</p>
+        <p class="list-group-item"><strong>Version:</strong>{{ __('messages.versionexp') }}</p>
+        <p class="list-group-item"><strong>PS:</strong>{{ __('messages.psexp') }}</p>
+        <p class="list-group-item"><strong>SKI:</strong>{{ __('messages.skiexp') }}</p>
+        <p class="list-group-item"><strong>WF:</strong> {{ __('messages.wfexp') }}</p>
+        <p class="list-group-item"><strong>WR:</strong> {{ __('messages.wrexp') }}</p>
+        <p class="list-group-item"><strong>PAC:</strong>{{ __('messages.pacexp') }}</p>
+        <p class="list-group-item"><strong>SHO:</strong>{{ __('messages.shoexp') }}</p>
+        <p class="list-group-item"><strong>PAS:</strong>{{ __('messages.pasexp') }}</p>
+        <p class="list-group-item"><strong>DRI:</strong>{{ __('messages.driexp') }}</p>
+        <p class="list-group-item"><strong>DEF:</strong> {{ __('messages.defexp') }}</p>
+        <p class="list-group-item"><strong>PHY:</strong> {{ __('messages.phyexp') }}</p>
+        <p class="list-group-item"><strong>Body:</strong> {{ __('messages.bodyexp') }}</p>
+        <p class="list-group-item"><strong>Popularity:</strong>{{ __('messages.popularityexp') }}</p>
+        <p class="list-group-item"><strong>BS:</strong> {{ __('messages.bsexp') }}</p>
+        <p class="list-group-item"><strong>IGS:</strong> {{ __('messages.igsexp') }}</p>
+        <p class="list-group-item"><strong>Club:</strong> {{ __('messages.clubexp') }}</p>
+        <p class="list-group-item"><strong>League:</strong> {{ __('messages.leagueexp') }}</p>
+        <p class="list-group-item"><strong>Country:</strong> {{ __('messages.countryexp') }}</p>
     </div>
 </div>
 @endsection
